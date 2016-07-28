60e871298146d5b672cfc2ac2967117f9dacf634
luisantonioig <c-remix@hotmail.es>
2016-07-28 13:23:15 -0500

    Listado de todo el texto de una pagina web

--- principal.py
+++ principal.py
@@ -4,6 +4,27 @@ import requests
 page = requests.get('https://www.tripadvisor.com.mx/Attractions-g445056-Activities-Sayulita_Pacific_Coast.html')
 tree = html.fromstring(page.content)
 
+#Obtenemos el texto de las etiquetas div
 divs = tree.xpath("//div/text()")
-divs.remove('\n')
-print divs[-3]
+while "\n" in divs:
+  divs.remove('\n')
+while " " in divs:
+  divs.remove(" ")
+
+#Obtenemos el texto de las etiquetas a
+a = tree.xpath("//a/text()")
+while "\n" in a:
+  a.remove('\n')
+while " " in a:
+  a.remove(" ")
+
+#Obtenemos el texto de las etiquetas spam
+spam = tree.xpath("//a/text()")
+while "\n" in spam:
+  spam.remove("\n")
+while " " in spam:
+  spam.remove(" ")
+
+print a
+print divs
+print spam
